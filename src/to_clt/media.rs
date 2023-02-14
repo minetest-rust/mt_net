402f7485a5d85b1efc4a76d2c3c80f295e9816a4
Lizzy Fleckenstein <eliasfleckenstein@web.de>
2023-02-14 16:25:05 +0100

    Implement TileAnim

--- src/to_clt/media.rs
+++ src/to_clt/media.rs
@@ -1,7 +1,17 @@
 use super::*;
 
-#[mt_derive(to = "clt")]
-pub struct TileAnim; // TODO
+#[mt_derive(to = "clt", repr = "u8", tag = "type")]
+pub enum TileAnim {
+    None = 0,
+    VerticalFrame {
+        n_frames: [u16; 2],
+        duration: f32,
+    },
+    SpriteSheet {
+        aspect_ratio: [u8; 2],
+        duration: f32,
+    },
+}
 
 #[mt_derive(to = "clt")]
 pub struct ItemDef; // TODO
